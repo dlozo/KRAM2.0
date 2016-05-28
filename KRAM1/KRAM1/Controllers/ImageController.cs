bb1f532410fe162ca25e2d051867f4ff9e377d3b
Rickard Wiesner <rickard.wiesner@hotmail.com>
2016-05-28 15:07:25 +0200

    En riktigt kass jsfil med nån ajaxskit som hämtar First() ur en lista som matchar det man skriver in i sökfunktionen, och en vanlig C# sökfunktion..

--- KRAM1/KRAM1/Controllers/ImageController.cs
+++ KRAM1/KRAM1/Controllers/ImageController.cs
@@ -8,31 +8,30 @@ using Microsoft.AspNet.Identity;
 using System.IO;
 using System.Web.Routing;
 using System.Drawing;
+using System.Web.Script.Serialization;
 
 namespace KRAM1.Controllers
-    {
+{
     public class ImageController : Controller
-        {
+    {
         ApplicationDbContext context = new ApplicationDbContext();
         // GET: Image
         public ActionResult Index()
-            {
+        {
             var model = context.Pictures.ToList();
             return View(model);
-            }
+        }
 
         // GET: Image/Details/5
         public ActionResult Details(int id)
-            {
-
+        {
             var list = context.Pictures.ToList();
 
-
             return View(list);
-            }
+        }
 
         public ActionResult Submit()
-            {
+        {
             ApplicationDbContext context = new ApplicationDbContext();
             var url = Request["imgurl"];
             var gettag = Request["tags"];
@@ -44,7 +43,6 @@ namespace KRAM1.Controllers
             }
             else
             {
-
                 //Hashtag hastags = new Hashtag() { Name = gettag };
                 //Picture newImage = new Picture()
                 //{
@@ -61,7 +59,7 @@ namespace KRAM1.Controllers
                     Picture newImage = new Picture()
                     {
                         PicUrl = url,
-                        TimeStamp = DateTime.Now,
+                        //TimeStamp = DateTime.Now,
                         UserId = userId,
                         Hashtag = hastags
                     };
@@ -73,94 +71,126 @@ namespace KRAM1.Controllers
                     Picture newImage = new Picture()
                     {
                         PicUrl = url,
-                        TimeStamp = DateTime.Now,
+                        // TimeStamp = DateTime.Now,
                         UserId = userId,
                         Hashtag = tag
                     };
                     context.Pictures.Add(newImage);
                     context.SaveChanges();
-
-
                 }
             }
-
             return View();
-            }
+        }
         public ActionResult FullImage(int fileName)
-            {
-
+        {
             var x = context.Pictures.Find(fileName);
-         
-            ViewBag.Hashtag=x.Hashtag;
-            ViewBag.x=x.PicUrl;
+
+            ViewBag.Hashtag = x.Hashtag;
+            ViewBag.x = x.PicUrl;
             return View();
-            }
+        }
         [HttpPost]
 
         public ActionResult Upload(Picture newPicture, HttpPostedFileBase file)
-            {
+        {
             int fileName = 0;
             ApplicationDbContext context = new ApplicationDbContext();
 
-                if(file==null)
-                    {
-                    ModelState.AddModelError("File", "Please Upload Your imgfile");
-                    }
-                    else
-                        {    
-                
-                        file.ValidateImageFile();
-                        string extension = Path.GetExtension(file.FileName);
-                        var tags = Request["tags"];
-               
-                        var userId = User.Identity.GetUserId();
-                     
+            if (file == null)
+            {
+                ModelState.AddModelError("File", "Please Upload Your imgfile");
+            }
+            else
+            {
+                file.ValidateImageFile();
+                string extension = Path.GetExtension(file.FileName);
+                var tags = Request["tags"];
 
-                        var fileNames = Path.GetFileName(file.FileName);
-                        var guid = Guid.NewGuid().ToString(); //Randomizer filnamnet
-                        var path = Path.Combine(Server.MapPath("~/uploads"), guid+fileNames); //Får fram fullständiga mappen man sparar i. Vi får ändra till server mappen senare.
-                       
-                        string fl = path.Substring(path.LastIndexOf("\\"));
-                        string[] split = fl.Split('\\');
+                var userId = User.Identity.GetUserId();
 
-                        string newpath = split[1];
-                      
-                        string imagepath = "~/uploads/"+newpath;
-                        Hashtag hastags = new Hashtag() { Name=tags };
-                        //Binder till bildtabellen i databasen
-                        newPicture.PicUrl=imagepath; //Får nog kanske göra om imagepath / path senare när vi laddar upp den till en server.
-                        newPicture.TimeStamp=DateTime.Now;
-                        newPicture.Hashtag=hastags;
-                        newPicture.UserId=userId;
-   
 
-                        file.SaveAs(path); //Sparar till en mapp ~/uploads/
-                        context.Pictures.Add(newPicture);
-                        context.SaveChanges();
-                         ModelState.Clear();
-                        ViewBag.Message = "Image uploaded successfully";
-                        fileName=newPicture.Id;
-                      return RedirectToAction("FullImage", "Image", new { fileName = fileName });
-                        }
-                    
+                var fileNames = Path.GetFileName(file.FileName);
+                var guid = Guid.NewGuid().ToString(); //Randomizer filnamnet
+                var path = Path.Combine(Server.MapPath("~/uploads"), guid + fileNames); //Får fram fullständiga mappen man sparar i. Vi får ändra till server mappen senare.
 
-                //TempData["Success"]="Upload successful";
-                    return RedirectToAction("Index");
-                }
+                string fl = path.Substring(path.LastIndexOf("\\"));
+                string[] split = fl.Split('\\');
+
+                string newpath = split[1];
+
+                string imagepath = "~/uploads/" + newpath;
+                Hashtag hastags = new Hashtag() { Name = tags };
+                //Binder till bildtabellen i databasen
+                newPicture.PicUrl = imagepath; //Får nog kanske göra om imagepath / path senare när vi laddar upp den till en server.
+                                               // newPicture.TimeStamp = DateTime.Now;
+                newPicture.Hashtag = hastags;
+                newPicture.UserId = userId;
+
+
+                file.SaveAs(path); //Sparar till en mapp ~/uploads/
+                context.Pictures.Add(newPicture);
+                context.SaveChanges();
+                ModelState.Clear();
+                ViewBag.Message = "Image uploaded successfully";
+                fileName = newPicture.Id;
+                return RedirectToAction("FullImage", "Image", new { fileName = fileName });
+            }
+
+            //TempData["Success"]="Upload successful";
+            return RedirectToAction("Index");
+        }
         public ActionResult HashImg(int fileName)
         {
-
             var x = context.Pictures.Find(fileName);
             ViewBag.hash = x.Hashtag.Picture;
-
 
             return View();
         }
 
+        public ActionResult HashtagSearch(string hashtag)
+        {
+            var hashtagResults = new List<Hashtag>();
 
-    }
-              
+            //http://stackoverflow.com/questions/26206288/entity-to-json-error-a-circular-reference-was-detected-while-serializing-an-ob
+            //Varför ProxyCreationEnabled behövs ^
+            context.Configuration.ProxyCreationEnabled = false;
 
+            foreach (var tag in context.Hashtags.ToList())
+            {
+                if (tag.Name.ToLower().Contains(hashtag.ToLower()))
+                {
+                    hashtagResults.Add(tag);
+                }
             }
-        
-    
+            if (hashtagResults.Count == 0)
+            {
+                return Json("No hashtag founds", JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(hashtagResults.First(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ImageSearch(string searchInput)
+        {
+            if (String.IsNullOrEmpty(searchInput))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                var pictureResults = new List<Picture>();
+
+                foreach (var picture in context.Pictures.ToList())
+                {
+                    if (picture.Hashtag.Name.ToLower().Contains(searchInput.ToLower()))
+                    {
+                        pictureResults.Add(picture);
+                    }
+                }
+                return View(pictureResults);
+            }
+        }
+    }
+}
+
+
---
+++ KRAM1/KRAM1/Scripts/AjaxSearch.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $('#searchInput').on('input', function () {
+        $('#searchResults').text('Fetching search predictions...');
+
+        $.ajax({
+            method: 'GET',
+            url: '/Image/HashtagSearch',
+            data: {
+                hashtag: $('#searchInput').val()
+            },
+            dataType: 'json',
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert('Något gick fel! status:' + textStatus + "\nerror: " + errorThrown);
+            },
+            success: function (data) {
+                if (data.Name === undefined)
+                    $('#searchResults').html("Undefined: " + data);
+                else
+                    $('#searchResults').html
+                        (data.Name);
+            }
+        });
+    });
+});
