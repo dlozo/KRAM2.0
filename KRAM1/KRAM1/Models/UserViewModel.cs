f8ee85cdaead15432de885177e61be2b06201a79
Mustafa Muhsin <teh.muzzi@gmail.com>
2016-05-30 12:57:59 +0200

    Fixat merge problem osv

--- KRAM1/KRAM1/Models/UserViewModel.cs
+++ KRAM1/KRAM1/Models/UserViewModel.cs
@@ -4,17 +4,16 @@ using System.Linq;
 using System.Web;
 
 namespace KRAM1.Models
-    {
+{
     public class UserViewModel
-        {
+    {
         public string UserID { get; set; }
         public string UserName { get; set; }
-		public string ProfilePic { get; set; }
-		public string UserEmail { get; set; }
-        public string PassWord { get; set; }
-        public int  Pictures { get; set; }
+        public string ProfilePic { get; set; }
 
+        public string UserEmail { get; set; }
+        public IList<Picture> Pictures { get; set; }
         public int TotalLikes { get; set; }
-    
-        }
+        public bool IsOwner { get; set; }
     }
+}
