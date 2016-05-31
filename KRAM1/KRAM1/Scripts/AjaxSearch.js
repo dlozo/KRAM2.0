3f55b1eeab58e85b2e756e9261bbcda2c308477a
Rickard W <rickard.wiesner@hotmail.com>
2016-05-31 12:32:16 +0200

    ajax search stuff.. not complete. writes out all "hashtags" in a string atm

--- KRAM1/KRAM1/Scripts/AjaxSearch.js
+++ KRAM1/KRAM1/Scripts/AjaxSearch.js
@@ -17,15 +17,22 @@
                 //$('#searchResults').each(function (i) For loop är bättre, fuck ".each"
                 for (var i = 0; i < data.length; i++) {
                     if (data[i].Name === undefined) {
-                        console.log(data);
-                        $('#searchResults').html("Undefined: " + data);
+                        console.log("123");
+                        $('#searchResults').html("No hashtags by that name: " + data);
                     }
+                        //"<br /> @Html.ActionLink(item.Name, 'HashtagSearch', 'Image', new { hashtag = " + data[i].Name + " }, null)" + data[i].Name;
                     else {
-                        arr1 += "<br /> " + data[i].Name;
+                        console.log("456");
+                        //arr1 += '@URL.Action("HashImg", "Image", new { fileName = "-1"})';
+                        //arr1 = arr1.replace("-1", data[i].Id)
+                        arr1 += "<br />" + data[i].Name;
+                        //  var url = '@Html.Raw(@Url.Action("ImageSearch",   "Image"))' + '?hashtag=' + data[i].Name;
+                        $('#searchResults').attr("href", "/Image/ImageSearch?searchInput=" + data[i].Name);//Skriver ut länk
+                        $('#searchResults').text('#' + data[i].Name);
                     };
                 } {
                 }
-                $('#searchResults').html(arr1);
+                $('#searchResults').html(arr1); //Skriver ut arr1 som har alla "hashtags" som data loopat igenom.
             }
         });
     });
