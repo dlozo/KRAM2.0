2439d7ab000316c7496ed879d563d47ead4400e1
Rickard Wiesner <rickard.wiesner@hotmail.com>
2016-06-03 00:19:43 +0200

    API skit som inte funkar alls, går inte att returnera Json när man ärver från ApiController. I Davids exempel har han bara ärvt utav Controller... är det ett Api då?

---
+++ KRAM1/KRAM1/Controllers/ImageApiController.cs
@@ -0,0 +1,49 @@
+﻿using KRAM1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+
+namespace KRAM1.Controllers
+{
+    public class ImageApiController : ApiController
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        // GET api/<controller>
+        public List<Picture> Get()
+        {
+            var allPics = context.Pictures.ToList();
+
+            return allPics;
+        }
+
+
+        // GET api/<controller>/5
+        public ActionResult Get(string searchString)
+        {
+            var allImages = context.Pictures.ToList();
+
+            var imagesBasedOnHashtag = allImages.Where(i => i.Hashtag.Name == searchString).ToList();
+            return Json(imagesBasedOnHashtag.First(), JsonRequestBehavior.AllowGet);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
