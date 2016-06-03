51e7978e2b376919e10a695217e1ba51d3f52b93
Rickard W <rickard.wiesner@hotmail.com>
2016-06-03 11:02:08 +0200

    lösenords regler för registreringen ändrad.

--- KRAM1/KRAM1/Scripts/AjaxSearch.js
+++ KRAM1/KRAM1/Scripts/AjaxSearch.js
@@ -14,23 +14,17 @@
             },
             success: function (data) {
                 var arr1 = "";
-                //$('#searchResults').each(function (i) For loop är bättre, fuck ".each"
                 for (var i = 0; i < data.length; i++) {
                     if (data[i].Name === undefined) {
                         console.log("123");
                         $('#searchResults').html("No hashtags by that name: " + data);
                     }
-                        //"<br /> @Html.ActionLink(item.Name, 'HashtagSearch', 'Image', new { hashtag = " + data[i].Name + " }, null)" + data[i].Name;
                     else {
-                        // test.push("<a href=/Image/ImageSearch?searchInput=" + data[i])
                         console.log("456");
                         arr1 += "<li>" + "<a href=/Image/ImageSearch?searchInput=" + data[i].Name + ">" + "#" + data[i].Name + "</a>" + "</li>";
                         $('#searchResults').attr("href", "/Image/ImageSearch?searchInput=" + data[i].Name);//Skriver ut länk
-                        //$('#searchResults').text('#' + data[i].Name);
-                        //$('#searchResults').text("<li><a href=/Image/ImageSearch?searchInput=" + +data[i].Name + "</a></li>");
                     };
                 }
-                // $('#searchResults').attr("<li>" + "<a href=/Image/ImageSearch?searchInput=" + data[i].Name + "</a>" + "</li>");
                 $('#searchResults').html(arr1); //Skriver ut arr1 som har alla "hashtags" som data loopat igenom.
             }
         });
